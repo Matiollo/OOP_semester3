72b4cf07b1a3c44400b5ececc5ec512278167ee4
Krasnoshapka Anastasiia <71903634+Matiollo@users.noreply.github.com>
2021-12-26 12:23:37 +0200

    lab5_finished

---
+++ lab5/task5/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Context context = new Context();
+
+        if(DateTime.Now.Hour >= 22 || DateTime.Now.Hour < 10)
+        {
+            context.AddStrategy(new Stairs());
+        }
+        else if(GetProphylacticWorksPresence())
+        {
+            context.AddStrategy(new Stairs());
+            context.AddStrategy(new Elevator());
+            context.AddStrategy(new Escalator());
+        }
+        else
+        {
+            int elevatorState = GetEquipmentState("elevator");
+            int escalatorState = GetEquipmentState("escalator");
+            if(elevatorState > escalatorState)
+            {
+                context.AddStrategy(new Stairs());
+                context.AddStrategy(new Elevator());
+            }
+            else
+            {
+                context.AddStrategy(new Stairs());
+                context.AddStrategy(new Escalator());
+            }
+        }
+
+        context.ExecuteStrategies();
+    }
+
+    static bool GetProphylacticWorksPresence()
+    {
+        bool prophylacticWorks;
+        while(true)
+        {
+            Console.WriteLine("Is there a prophylactic work today? Y/N");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if(key.Key == ConsoleKey.Y)
+            {
+                prophylacticWorks = true;
+                break;
+            }
+            if(key.Key == ConsoleKey.N)
+            {
+                prophylacticWorks = false;
+                break;
+            }
+            Console.WriteLine("Press either Y or N");
+        }
+        Console.WriteLine();
+        return prophylacticWorks;
+    }
+
+    static int GetEquipmentState(string equipment)
+    {
+        Console.WriteLine($"What state is {equipment} in?");
+        string stateStr = Console.ReadLine();
+        int state;
+        if(int.TryParse(stateStr, out state) && state >= 0)
+        {
+            return state;
+        }
+        Console.WriteLine($"Enter integer positive value.");
+        return GetEquipmentState(equipment);
+    }
+}
+
+abstract class Strategy
+{
+    public abstract void TransferBetweenFloors();
+}
+
+class Stairs : Strategy
+{
+    public override void TransferBetweenFloors()
+    {
+        Console.WriteLine("Customers transfer between floors by stairs.");
+    }
+}
+
+class Elevator : Strategy
+{
+    public override void TransferBetweenFloors()
+    {
+        Console.WriteLine("Customers transfer between floors by elevator.");
+    }
+}
+
+class Escalator : Strategy
+{
+    public override void TransferBetweenFloors()
+    {
+        Console.WriteLine("Customers transfer between floors by escalator.");
+    }
+}
+
+class Context
+{
+    private List<Strategy> strategies;
+
+    public Context()
+    {
+        this.strategies = new List<Strategy>();
+    }
+
+    public bool AddStrategy(Strategy strategy)
+    {
+        foreach(Strategy str in this.strategies)
+        {
+            if(str == strategy)
+            {
+                return false;
+            }
+        }
+        this.strategies.Add(strategy);
+        return true;
+    }
+
+    public bool RemoveStrategy(Strategy strategy)
+    {
+        return this.strategies.Remove(strategy);
+    }
+
+    public void ExecuteStrategies()
+    {
+        foreach(Strategy strategy in this.strategies)
+        {
+            strategy.TransferBetweenFloors();
+        }
+    }
+}
---
+++ lab5/task6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Handler thirdShift = new Handler(22, 7, null);
+        Handler secondShift = new Handler(14, 23, thirdShift);
+        Handler firstShift = new Handler(6, 15, secondShift);
+        thirdShift.NextHandler = firstShift;
+
+        string data = "reguest";
+        string response;
+
+        if(DateTime.Now.Hour >= 7 || DateTime.Now.Hour < 15)
+        {
+            response = firstShift.HandleRequest(data);
+        }
+        else if(DateTime.Now.Hour >= 15 || DateTime.Now.Hour < 23)
+        {
+            response = secondShift.HandleRequest(data);
+        }
+        else
+        {
+            response = thirdShift.HandleRequest(data);
+        }
+
+        Console.WriteLine(response);
+    }
+}
+
+class Handler
+{
+    private Handler next;
+    private int startHour;
+    private int finishHour;
+
+    public Handler NextHandler
+    {
+        set
+        {
+            next = value;
+        }
+    }
+
+    public Handler(int startHour, int finishHour, Handler nextHandler)
+    {
+        this.startHour = startHour;
+        if(startHour < 0 || startHour > 23)
+        {
+            this.startHour = 6;
+        }
+        this.finishHour = finishHour;
+        if(finishHour < 0 || finishHour > 23)
+        {
+            this.finishHour = 15;
+        }
+        next = nextHandler;
+    }
+
+    public string HandleRequest(string data)
+    {
+        if (DateTime.Now.Hour >= this.startHour && DateTime.Now.Hour < this.finishHour - 1)
+            return $"Request handled by the shift that works from {this.startHour} till {this.finishHour}.";
+        else if (next != null)
+        {
+            return "Request was handed to the next shift.\r\n" + next.HandleRequest(data);
+        }
+        return "Next handler unspecified. Request not handled.";
+    }
+}
+
+
